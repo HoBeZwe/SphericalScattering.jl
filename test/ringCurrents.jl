6b297312c22d4921036fd195d13d6dfe2fe53919
Bernd Hofmann <bernd.hofmann@tum.de>
2022-11-06 17:59:28 +0100

    tests for ring currents via comparison with BEAST

--- test/ringCurrents.jl
+++ test/ringCurrents.jl
@@ -2,113 +2,135 @@
 
 @testset "Electric ring current" begin
 
-    ex = electricRingCurrent(; wavenumber=30.0, center=SVector(0.0, 0.0, 2.0), radius=0.5) # ≈ 10 MHz
+    f = 1e8
+    κ = 2π * f / c   # Wavenumber
+
+
+    ex = electricRingCurrent(; wavenumber=κ, center=SVector(0.0, 0.0, 2.0), radius=0.5)
 
     @testset "Incident fields" begin
 
         # define an observation point
         point_cart = [SVector(4.0, 2.0, 3.2)]
 
-        E  = field(ex, ElectricField(point_cart))
-        H  = field(ex, MagneticField(point_cart))
+        EF = field(ex, ElectricField(point_cart))
+        HF = field(ex, MagneticField(point_cart))
         FF = field(ex, FarField(point_cart))
 
-        # TODO: find better way of verifying (via BEAST)
-        # @test FF[1][1] ≈ 295.0240872654143 + 112.00825545163434im
-        # @test FF[1][2] ≈ 295.0240872654143 + 112.00825545163434im
-        # @test FF[1][3] ≈ -368.78010908176793 - 140.01031931454293im
+        @test FF[1][1] ≈ 7.942044920729696 - 3.1030988210509873im
+        @test FF[1][2] ≈ -15.88408984145939 + 6.206197642101974im
+        @test FF[1][3] ≈ 0.0 + 0.0im
 
-        # @test E[1][1] ≈ 33.75576840234728 - 66.33989918223259im
-        # @test E[1][2] ≈ 33.75576840234728 - 66.33989918223259im
-        # @test E[1][3] ≈ -118.11657839808761 + 218.13026261521526im
+        @test EF[1][1] ≈ -7.942044920729696 + 3.1030988210509873im
+        @test EF[1][2] ≈ 15.88408984145939 - 6.206197642101974im
+        @test EF[1][3] ≈ 0.0 + 0.0im
 
-        # @test H[1][1] ≈ -0.23911545354270725 + 0.4457313513337625im
-        # @test H[1][2] ≈ 0.23911545354270725 - 0.4457313513337625im
-        # @test H[1][3] ≈ 0.0 + 0.0im
-        @test 1 == 1
+        @test HF[1][1] ≈ -0.01019515697859331 + 0.00628333403389956im
+        @test HF[1][2] ≈ -0.0050975784892966555 + 0.003141667016949781im
+        @test HF[1][3] ≈ 0.04525565726450052 - 0.017077464855190773im
     end
 
     @testset "Scattered fields" begin
 
-        # define scatterer: PEC sphere
-        sp = PECSphere(; radius=1.0)
+        # ----- BEAST solution
+        𝐸 = ex
 
-        # define an observation point
-        point_cart = [SVector(4.0, 2.0, 3.2)]
+        𝑒 = n × 𝐸 × n
+        𝑇 = Maxwell3D.singlelayer(; wavenumber=κ)
 
-        # compute scattered fields
-        E  = scatteredfield(sp, ex, ElectricField(point_cart))
-        H  = scatteredfield(sp, ex, MagneticField(point_cart))
-        FF = scatteredfield(sp, ex, FarField(point_cart))
+        e = assemble(𝑒, RT)
+        T = assemble(𝑇, RT, RT)
 
-        # @test E[1][1] ≈ -5.502103327427189 - 7.627682569142905im
-        # @test E[1][2] ≈ -5.502103327427188 - 7.6276825691429035im
-        # @test E[1][3] ≈ 8.424633687603194 + 12.484440209052982im
+        u = T \ e
 
-        # @test H[1][1] ≈ -0.021517831140115615 - 0.030977247907197428im
-        # @test H[1][2] ≈ 0.021517831140115618 + 0.03097724790719743im
-        # @test H[1][3] ≈ 0.0 + 0.0im
+        EF_MoM = +potential(MWSingleLayerField3D(; wavenumber=κ), points_cartNF, u, RT)
+        HF_MoM = -potential(MWDoubleLayerField3D(; wavenumber=κ), points_cartNF, u, RT)
+        FF_MoM = -im * f / (2 * c) * potential(MWFarField3D(; gamma=𝑇.gamma), points_cartFF, u, RT)
 
-        # @test FF[1][1] ≈ 14.44466970131592 + 31.74394205284891im
-        # @test FF[1][2] ≈ 14.444669701315918 + 31.743942052848908im
-        # @test FF[1][3] ≈ -18.055837126644896 - 39.67992756606113im
-        @test 1 == 1
+
+        # ----- this package
+        sp = PECSphere(; radius=spRadius)
+
+        EF = scatteredfield(sp, ex, ElectricField(points_cartNF))
+        HF = scatteredfield(sp, ex, MagneticField(points_cartNF)) * c * 𝜇
+        FF = scatteredfield(sp, ex, FarField(points_cartFF))
+
+
+        # ----- compare
+        diff_EF = norm.(EF - EF_MoM) ./ maximum(norm.(EF))  # worst case error
+        diff_HF = norm.(HF - HF_MoM) ./ maximum(norm.(HF))  # worst case error
+        diff_FF = norm.(FF - FF_MoM) ./ maximum(norm.(FF))  # worst case error
+
+        @test maximum(20 * log10.(abs.(diff_EF))) < -24 # dB 
+        @test maximum(20 * log10.(abs.(diff_HF))) < -24 # dB
+        @test maximum(20 * log10.(abs.(diff_FF))) < -24 # dB
     end
 end
 
 
 @testset "Magnetic ring current" begin
 
-    ex = magneticRingCurrent(; wavenumber=30.0, center=SVector(0.0, 0.0, 2.0), radius=0.5) # ≈ 10 MHz
+    f = 1e8
+    κ = 2π * f / c   # Wavenumber
+
+
+    ex = magneticRingCurrent(; wavenumber=κ, center=SVector(0.0, 0.0, 2.0), radius=0.5)
 
     @testset "Incident fields" begin
 
         # define an observation point
         point_cart = [SVector(4.0, 2.0, 3.2)]
 
-        E  = field(ex, ElectricField(point_cart))
-        H  = field(ex, MagneticField(point_cart))
+        EF = field(ex, ElectricField(point_cart))
+        HF = field(ex, MagneticField(point_cart))
         FF = field(ex, FarField(point_cart))
 
-        # TODO: find better way of verifying (via BEAST)
-        # @test FF[1][1] ≈ 295.0240872654143 + 112.00825545163434im
-        # @test FF[1][2] ≈ 295.0240872654143 + 112.00825545163434im
-        # @test FF[1][3] ≈ -368.78010908176793 - 140.01031931454293im
+        @test FF[1][1] ≈ -5.595916238822697e-5 + 2.186424435609382e-5im
+        @test FF[1][2] ≈ 0.00011191832477645392 - 4.3728488712187633e-5im
+        @test FF[1][3] ≈ 0.0 + 0.0im
 
-        # @test E[1][1] ≈ 33.75576840234728 - 66.33989918223259im
-        # @test E[1][2] ≈ 33.75576840234728 - 66.33989918223259im
-        # @test E[1][3] ≈ -118.11657839808761 + 218.13026261521526im
+        @test EF[1][1] ≈ 0.01019515697859331 - 0.00628333403389956im
+        @test EF[1][2] ≈ 0.0050975784892966555 - 0.003141667016949781im
+        @test EF[1][3] ≈ -0.04525565726450052 + 0.017077464855190773im
 
-        # @test H[1][1] ≈ -0.23911545354270725 + 0.4457313513337625im
-        # @test H[1][2] ≈ 0.23911545354270725 - 0.4457313513337625im
-        # @test H[1][3] ≈ 0.0 + 0.0im
-        @test 1 == 1
+        @test HF[1][1] ≈ -5.595916238822697e-5 + 2.186424435609382e-5im
+        @test HF[1][2] ≈ 0.00011191832477645392 - 4.3728488712187633e-5im
+        @test HF[1][3] ≈ 0.0 + 0.0im
     end
 
     @testset "Scattered fields" begin
 
-        # define scatterer: PEC sphere
-        sp = PECSphere(; radius=1.0)
+        # ----- BEAST solution
+        𝐸 = ex
 
-        # define an observation point
-        point_cart = [SVector(4.0, 2.0, 3.2)]
+        𝑒 = n × 𝐸 × n
+        𝑇 = Maxwell3D.singlelayer(; wavenumber=κ)
 
-        # compute scattered fields
-        E  = scatteredfield(sp, ex, ElectricField(point_cart))
-        H  = scatteredfield(sp, ex, MagneticField(point_cart))
-        FF = scatteredfield(sp, ex, FarField(point_cart))
+        e = assemble(𝑒, RT)
+        T = assemble(𝑇, RT, RT)
 
-        # @test E[1][1] ≈ -5.502103327427189 - 7.627682569142905im
-        # @test E[1][2] ≈ -5.502103327427188 - 7.6276825691429035im
-        # @test E[1][3] ≈ 8.424633687603194 + 12.484440209052982im
+        u = T \ e
 
-        # @test H[1][1] ≈ -0.021517831140115615 - 0.030977247907197428im
-        # @test H[1][2] ≈ 0.021517831140115618 + 0.03097724790719743im
-        # @test H[1][3] ≈ 0.0 + 0.0im
+        EF_MoM = -potential(MWSingleLayerField3D(; wavenumber=κ), points_cartNF, u, RT)
+        HF_MoM = +potential(MWDoubleLayerField3D(; wavenumber=κ), points_cartNF, u, RT)
+        FF_MoM = +im * f / (2 * c) * potential(MWFarField3D(; gamma=𝑇.gamma), points_cartFF, u, RT)
 
-        # @test FF[1][1] ≈ 14.44466970131592 + 31.74394205284891im
-        # @test FF[1][2] ≈ 14.444669701315918 + 31.743942052848908im
-        # @test FF[1][3] ≈ -18.055837126644896 - 39.67992756606113im
-        @test 1 == 1
+
+        # ----- this package
+        sp = PECSphere(; radius=spRadius)
+
+        EF = scatteredfield(sp, ex, ElectricField(points_cartNF))
+        HF = scatteredfield(sp, ex, MagneticField(points_cartNF)) * c * 𝜇
+        FF = scatteredfield(sp, ex, FarField(points_cartFF))
+
+
+        # ----- compare
+        diff_EF = norm.(EF - EF_MoM) ./ maximum(norm.(EF))  # worst case error
+        diff_HF = norm.(HF - HF_MoM) ./ maximum(norm.(HF))  # worst case error
+        diff_FF = norm.(FF - FF_MoM) ./ maximum(norm.(FF))  # worst case error
+
+        @test maximum(20 * log10.(abs.(diff_EF))) < -24 # dB 
+        @test maximum(20 * log10.(abs.(diff_HF))) < -24 # dB
+        @test maximum(20 * log10.(abs.(diff_FF))) < -24 # dB
     end
 end
