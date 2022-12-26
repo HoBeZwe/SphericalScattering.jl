7f4ee9497c4672948aa9344368e141ffee9d4489
Bernd Hofmann <bernd.hofmann@tum.de>
2022-12-26 22:42:24 +0100

    fix links in docs

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,6 @@ DocMeta.setdocmeta!(SphericalScattering, :DocTestSetup, :(using SphericalScatter
 makedocs(;
     modules=[SphericalScattering],
     authors="Bernd Hofmann <Bernd.Hofmann@tum.de> and contributors",
-    repo="https://github.com/HoBeZwe/SphericalScattering.jl/tree/master",
     sitename="SphericalScattering.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true", canonical="https://HoBeZwe.github.io/SphericalScattering.jl", assets=String[]
